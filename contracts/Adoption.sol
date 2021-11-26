d925c8c5ca416d8e5ba3330973fd35efbe864f84
vortex200 <julijus.rancevas@gmail.com>
2021-11-26 18:27:15 +0200

    Update

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,7 +2,31 @@ pragma solidity ^0.5.0;
 
 contract Adoption {
 
+struct art{
+      address owner;
+      string url;
+      int256 price;
+      string author;
+}
+
+art[] public artworks;
+uint256 public totalArtworks;
+
+constructor() public {
+    totalArtworks = 0;
+}
+
 address[16] public adopters;
+
+function create(string memory url, int256 price, string memory author) public returns (uint256 totalCountries){
+    address owner = msg.sender;
+    art memory newArtwork = art(owner, url, price, author);
+    artworks.push(newArtwork);
+    totalArtworks++;
+    //emit event
+    // emit CountryEvent (countryName, leader, population);
+    return totalArtworks;
+}
 
 // Adopting a pet
 function adopt(uint petId) public returns (uint) {
