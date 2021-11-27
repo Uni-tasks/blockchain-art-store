6b037c6bcb66b26b3b2d1613320686119a534835
vortex200 <julijus.rancevas@gmail.com>
2021-11-27 19:08:21 +0200

    artwork

--- src/js/app.js
+++ src/js/app.js
@@ -51,8 +51,6 @@ App = {
 
   displayWallet: function() {
     // Display user wallet address
-    console.log("User wallet  address:")
-    console.log(App.web3Provider.selectedAddress);
     $('#wallet-address').text(App.web3Provider.selectedAddress);
   },
 
@@ -65,7 +63,7 @@ App = {
       // Set the provider for our contract
       App.contracts.Adoption.setProvider(App.web3Provider);
 
-      App.logArtworks();
+      App.loadArtworks();
       // Use our contract to retrieve and mark the adopted pets
       return App.markAdopted();
     });
@@ -76,6 +74,8 @@ App = {
   bindEvents: function() {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
     $(document).on('click', '#new-artwork-btn', App.handleCreateArtwork);
+    $(document).on('click', '.sale-btn', App.handleSellArtwork);
+    $(document).on('click', '.delete-btn', App.handleDeleteArtwork);
   },
 
   markAdopted: function() {
@@ -104,7 +104,7 @@ App = {
     });
   },
 
-  logArtworks: function() {
+  loadArtworks: function() {
     var adoptionInstance;
 
     App.contracts.Adoption.deployed().then(function(instance) {
@@ -114,13 +114,55 @@ App = {
     }).then(function(artworks) {
       count = artworks.c[0]
 
-      try {
-        for (i = 0; i < count; i++) {
-          adoptionInstance.getArtwork.call(i).then(function(artwork) {
-            console.log(artwork);
-          });
-      }} catch (error) {
-        console.log(error);
+      var artworksRow = $('#artworksRow');
+      var userArtworksRow = $('#userArtworksRow');
+      var artworkTemplate = $('#artworkTemplate');
+      var userArtworkTemplate = $('#userArtworkTemplate');
+
+      // for (i = 0; i < data.length; i ++) {
+      //   petTemplate.find('.panel-title').text(data[i].name);
+      //   petTemplate.find('img').attr('src', data[i].picture);
+      //   petTemplate.find('.pet-breed').text(data[i].breed);
+      //   petTemplate.find('.pet-age').text(data[i].age);
+      //   petTemplate.find('.pet-location').text(data[i].location);
+      //   petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+      //   artworksRow.append(petTemplate.html());
+      // }
+
+      for (i = 0; i < count; i++) {
+        adoptionInstance.getArtwork.call(i).then(function(artwork) {
+          const id = artwork[0].c[0];
+          const url = artwork[1];
+          const isForSale = artwork[2];
+          const price = artwork[3].c[0];
+          const ownerAddress = artwork[4];
+          const userAddress = App.web3Provider.selectedAddress;
+
+          if(url !== '') {
+            artworkTemplate.find('img').attr('src', url);
+
+            if (isForSale) {
+              artworkTemplate.find('.artwork-price').text(price + '$');
+              const text = ownerAddress === userAddress ? 'Your artwork' : 'Purchase';
+              artworkTemplate.find('.purchase-btn').text(text).attr('disabled', ownerAddress === userAddress ? true : false);;
+            } else {
+              artworkTemplate.find('.artwork-price').text('');
+              artworkTemplate.find('.purchase-btn').text('Not for sale').attr('disabled', true);
+            }
+
+            if (ownerAddress === userAddress) {
+              userArtworkTemplate.find('img').attr('src', url);
+              userArtworkTemplate.find('.sale-btn').attr('data-id', id);
+              userArtworkTemplate.find('.card-body').attr('data-id', id);
+              userArtworksRow.append(userArtworkTemplate.html());
+            }
+            
+            artworksRow.append(artworkTemplate.html()); 
+          }
+        }).catch(function(err) {
+          console.log(err.message);
+        });
       }
 
     }).catch(function(err) {
@@ -168,11 +210,69 @@ App = {
       }
 
       var account = accounts[0];
+      console.log(account);
 
       App.contracts.Adoption.deployed().then(function(instance) {
         adoptionInstance = instance;
 
         return adoptionInstance.create(url, {from: account});
+      }).then(function(result) {
+        $('#url').val('');
+        location.reload();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  handleSellArtwork: function(event) {
+    event.preventDefault();
+
+    const parentElement = $(event.target.parentElement)
+    var price = parseInt(parentElement.find('input').val());
+    var artworkId = parseInt(parentElement.data('id'));
+
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.sellArtwork(artworkId, price, {from: account});
+      }).then(function(result) {
+        $('#url').val('');
+        location.reload();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  handleDeleteArtwork: function(event) {
+    event.preventDefault();
+
+    var artworkId = parseInt($(event.target.parentElement).data('id'));
+    console.log(artworkId);
+
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.destroy(artworkId, {from: account});
       }).then(function(result) {
         $('#url').val('');
         location.reload();
