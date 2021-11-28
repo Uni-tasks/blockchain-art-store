0e882c7520acf095be258d005c1ccc0940cc7f5e
vortex200 <julijus.rancevas@gmail.com>
2021-11-28 12:35:17 +0200

    Remove pets

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -13,8 +13,6 @@ contract Adoption {
   Artwork[] public artworks;
   uint public nextId;
 
-  address[16] public adopters;
-
   function create(string memory name, string memory url) public {
       artworks.push(Artwork(nextId, name, url, false, 0, msg.sender));
       nextId++;
@@ -48,18 +46,4 @@ contract Adoption {
   function getTotalArtworks() public view returns (uint256 length){
       return artworks.length;
    }
-
-  // Adopting a pet
-  function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <= 15);
-
-    adopters[petId] = msg.sender;
-
-    return petId;
-  }
-
-  // Retrieving the adopters
-  function getAdopters() public view returns (address[16] memory) {
-    return adopters;
-  }
 }
